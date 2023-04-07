d9ec6c9c5de7c96675d101ba7ff97d29d77539e1
easonnong <nongeason@gmail.com>
2023-04-07 17:40:09 +0800

    init

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract ExchangeScript is Script {
     function setUp() public {}
 
     function run() public {
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,13 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/Exchange.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract ExchangeTest is Test {
+    Exchange public exchange;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        exchange = new Exchange();
     }
 }
