55fe138e616ef7037d060eebcb90dfc67b3e4903
easonnong <nongeason@gmail.com>
2023-04-08 09:35:35 +0800

    chore: forge init

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract ExchangeScript is Script {
+contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
